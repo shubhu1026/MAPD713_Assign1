75df603c46d2323576e50f66046a5edc97abd47b
shubhu1026 <shubhu.p2000@gmail.com>
2023-09-29 20:05:55 -0400

    Setup a server listening at port 5000

---
+++ index.js
@@ -0,0 +1,12 @@
+const SERVER_NAME = "products-api";
+const PORT = 5000;
+const HOST = "127.0.0.1";
+
+let errors = require("restify-errors");
+let restify = require("restify"),
+  productsSave = require("save")("products"),
+  server = restify.createServer({ name: SERVER_NAME });
+
+server.listen(PORT, HOST, function () {
+  console.log("Server %s listening at %s", server.name, server.url);
+});
