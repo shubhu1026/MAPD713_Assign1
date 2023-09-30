b9da966a82d7b1fae5e2b458069729c375438114
shubhu1026 <shubhu.p2000@gmail.com>
2023-09-29 20:11:46 -0400

    Added GET request

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Server constants
 const SERVER_NAME = "products-api";
 const PORT = 5000;
 const HOST = "127.0.0.1";
@@ -7,6 +8,24 @@ let restify = require("restify"),
   productsSave = require("save")("products"),
   server = restify.createServer({ name: SERVER_NAME });
 
+// Set the server to listen at localhost port 5000
 server.listen(PORT, HOST, function () {
   console.log("Server is listening at %s", server.name, server.url);
+});
+
+server.use(restify.plugins.fullResponse());
+server.use(restify.plugins.bodyParser());
+
+//--------------------------------------------------------------------------------//
+// GET REQUEST
+//--------------------------------------------------------------------------------//
+server.get("/products", function (req, res, next) {
+  console.log("products GET: received request");
+
+  // Find all the products within the given collection
+  productsSave.find({}, function (error, products) {
+    // Return all of the products in the system
+    console.log("products GET: sending response");
+    res.send(products);
+  });
 });
