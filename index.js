462ebe6f7ee42ace711dfc9e19b88d344b2f44f2
shubhu1026 <shubhu.p2000@gmail.com>
2023-09-30 16:05:07 -0400

    Added PUT Request for updating existing products

--- index.js
+++ index.js
@@ -70,8 +70,7 @@ server.get("/products/:id", function (req, res, next) {
 // POST REQUEST
 //--------------------------------------------------------------------------------//
 server.post("/products", function (req, res, next) {
-  //console.log("POST /product params=>" + JSON.stringify(req.params));
-  //console.log("POST /product body=>" + JSON.stringify(req.body));
+  console.log("products GET: received request");
 
   // validation of manadatory fields
   if (req.body.productId === undefined) {
@@ -103,8 +102,56 @@ server.post("/products", function (req, res, next) {
     // If there are any errors, pass them to next in the correct format
     if (error) return next(new Error(JSON.stringify(error.errors)));
 
+    postCount++;
+    console.log("products POST: sending response");
+    console.log(
+      "Processed Request Count--> Get:" + getCount + ", Post:" + postCount
+    );
+
     // Send the product if no issues
     res.send(201, product);
+  });
+});
+
+//--------------------------------------------------------------------------------//
+// PUT REQUEST
+//--------------------------------------------------------------------------------//
+server.put("/products/:id", function (req, res, next) {
+  console.log("products PUT: received request");
+
+  // validation of manadatory fields
+  if (req.body.productId === undefined) {
+    // If there are any errors, pass them to next in the correct format
+    return next(new errors.BadRequestError("Product Id must be supplied"));
+  }
+  if (req.body.price === undefined) {
+    // If there are any errors, pass them to next in the correct format
+    return next(new errors.BadRequestError("Price must be supplied"));
+  }
+  if (req.body.name === undefined) {
+    // If there are any errors, pass them to next in the correct format
+    return next(new errors.BadRequestError("Name must be supplied"));
+  }
+  if (req.body.quantity === undefined) {
+    // If there are any errors, pass them to next in the correct format
+    return next(new errors.BadRequestError("Quantity must be supplied"));
+  }
+  let newProduct = {
+    _id: req.params.id,
+    productId: req.body.productId,
+    name: req.body.name,
+    price: req.body.price,
+    quantity: req.body.quantity,
+  };
+
+  // Update the product with the persistence engine
+  productsSave.update(newProduct, function (error, product) {
+    // If there are any errors, pass them to next in the correct format
+    if (error) return next(new Error(JSON.stringify(error.errors)));
+
+    console.log("products PUT: sending response");
+    // Send a 200 OK response
+    res.send(200, newProduct);
   });
 });
 
